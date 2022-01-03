c95b8e5f41ad142f249c710cf958e0fb016d9e7a
mariandotg <marianguillaume.m@gmail.com>
2022-01-03 10:13:52 -0300

    Added author, tags, and a button to generate a new random quote

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
   const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [tags, setTags] = useState("");
 
   const baseURL = "https://api.quotable.io/"
 
@@ -14,6 +16,8 @@ const Home: NextPage = () => {
     try {
       const res = await axios.get(`${baseURL}random`);
       setQuote(res.data.content);
+      setAuthor(res.data.author);
+      setTags(res.data.tags);
     } catch (error) {
       console.log(error)
     }
@@ -26,7 +30,10 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
+        <button onClick={getQuote}>New quote</button>
         <h1>{quote}</h1>
+        <p>{author}</p>
+        <p>{tags}</p>
       </main>
 
       <footer className={styles.footer}>
