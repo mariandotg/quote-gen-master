aeb76a3d8396ca6403088b533481e60ddf4732fa
mariandotg <marianguillaume.m@gmail.com>
2022-01-03 09:40:22 -0300

    Added first API call, get random quote

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,30 @@
 import React, { ReactElement } from "react";
-import Document, { Html, Head, Main, NextScript, DocumentInitialProps, DocumentContext } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentInitialProps,
+  DocumentContext,
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 // NEXT.JS CUSTOM DOCUMENT
 // https://nextjs.org/docs/advanced-features/custom-document
 
 export default class MyDocument extends Document {
-  
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) =>
-          sheet.collectStyles(<App {...props} />),
-      });
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
       const initialProps = await Document.getInitialProps(ctx);
       return {
@@ -34,10 +42,12 @@ export default class MyDocument extends Document {
   }
 
   render(): ReactElement {
-    return(
-      <Html lang="en">
+    return (
+      <Html lang="es">
         <Head>
-          // SOME HEAD ELEMENTS
+          <title>Quote</title>
+          <meta name="description" content="Random famous quotes generator" />
+          <link rel="icon" href="/favicon.png" />
         </Head>
         <body>
           <Main />
